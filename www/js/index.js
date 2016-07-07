375cc0c38686ec63d043cc536ed6850bfe0c036b
devesh <devesh@yahoo-inc.com>
2016-07-07 11:01:46 -0700

    markup changes

--- www/js/index.js
+++ www/js/index.js
@@ -1,19 +1,28 @@
+var App = angular.module('ecp', []);
+
 var app = {
     initialize: function() {
         this.bindEvents();
     },
 
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        //document.addEventListener('offline', this.onDeviceReady, false);
+        //document.addEventListener('online', reloadpage, false);
     },
 
     onDeviceReady: function() {
         navigator.splashscreen.hide();
         app.receivedEvent('deviceready');
+
+        App.controller('ECPController', function($scope) {
+            $scope.ecpapiresponse = [
+                {"number": 9}
+            ];
+        });
     },
 
     receivedEvent: function(id) {
     }
 };
+
